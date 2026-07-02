dd79788cf9af2b9b48b443f8bdc23e9825336066
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-07-02 15:28:18 -0400

    achieve with lines.

--- p5/sketches/260702_recursive-half-lines/sketch.js
+++ p5/sketches/260702_recursive-half-lines/sketch.js
@@ -10,14 +10,13 @@ the line is perpendicular to the previous one.
 function setup() {
   // createCanvas(1000, 562); //in 16:9 aspect ratio.
   createCanvas(800, 800); //square.
-  angleMode (DEGREES);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(255);
 
   let l = 100;
-
   let start = createVector(100, 100);
 
   draw_line(start, l);
@@ -25,16 +24,31 @@ function draw() {
   noLoop();
 }
 
-function draw_line(start, l, state = -1) {
-  //vis settings:
-  strokeWeight(1);
-  stroke(0);
-  
-  line(start.x, start.y, start.x + l, start.y);
+const min = 1;
+const shrink = 0.9;
 
-  push();
-  translate (start.x + l, start.y); 
-  rotate (360); 
-  line (0,0,0,l/2); 
-  pop();
+function draw_line(start, l, state = 0) {
+  if (l <= min) return;
+
+  let end;
+
+  switch (state) {
+    case 0:
+      end = createVector(start.x + l, start.y);
+      break;
+    case 1:
+      end = createVector(start.x, start.y + l);
+      break;
+    case 2:
+      end = createVector(start.x - l, start.y);
+      break;
+    case 3:
+      end = createVector(start.x, start.y - l);
+      break;
+  }
+
+  line(start.x, start.y, end.x, end.y);
+
+  state = (state + 1) % 4;
+  draw_line(end, l * shrink, state);
 }
