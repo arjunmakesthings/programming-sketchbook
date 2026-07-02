2f645a1b4a52f08fa12f3c19ba1c0bf4bfc93ba3
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-07-02 16:08:39 -0400

    interim

--- p5/sketches/260702_recursive-half-lines/sketch.js
+++ p5/sketches/260702_recursive-half-lines/sketch.js
@@ -16,18 +16,24 @@ function setup() {
 function draw() {
   background(255);
 
-  let l = 100;
-  let start = createVector(100, 100);
+  const padding = 50;
 
-  draw_line(start, l);
+  let l = 100;
+
+  for (let x = 0; x <= width; x += l) {
+    for (let y = 0; y <= height; y += l) {
+      let start = createVector(x, y);
+      draw_points(start, l);
+    }
+  }
 
   noLoop();
 }
 
-const min = 1;
-const shrink = 0.9;
+const min = 0.2;
+const shrink = 0.955;
 
-function draw_line(start, l, state = 0) {
+function draw_points(start, l, state = 0) {
   if (l <= min) return;
 
   let end;
@@ -47,8 +53,72 @@ function draw_line(start, l, state = 0) {
       break;
   }
 
-  line(start.x, start.y, end.x, end.y);
+  let steps = max(abs(end.x - start.x), abs(end.y - start.y));
+
+  const cx = (start.x + end.x) * random();
+  const cy = (start.y + end.y) * random();
+
+  const maxD = dist(width * 0.5, height * 0.5, 0, 0);
+
+  push();
+  translate(cx, cy);
+  const ang = random(-2, 2);
+  rotate(ang);
+  for (let i = 0; i <= steps; i++) {
+    let x = lerp(start.x, end.x, i / steps);
+    let y = lerp(start.y, end.y, i / steps);
+
+    let n =
+      noise(frameCount * x * y) * dist(x, y, width / 2, height / 2) * 0.0095;
+    const lx = x - cx;
+    const ly = y - cy;
+
+    // strokeWeight (lx * ly /10500);
+
+    const px = round(x - cx + n);
+    const py = round(y - cy + n);
+
+    const wx = cx + px * cos(ang) - py * sin(ang);
+    const wy = cy + px * sin(ang) + py * cos(ang);
+
+    const d = dist(wx, wy, width * 0.5, height * 0.5);
+
+    strokeWeight(map(d, 0, maxD, 1.5, 0.75));
+
+    point(round(lx + n), round(ly + n));
+  }
+  pop();
 
   state = (state + 1) % 4;
-  draw_line(end, l * shrink, state);
+  draw_points(end, l * shrink, state);
+}
+
+// function draw_line(start, l, state = 0) {
+//   if (l <= min) return;
+
+//   let end;
+
+//   switch (state) {
+//     case 0:
+//       end = createVector(start.x + l, start.y);
+//       break;
+//     case 1:
+//       end = createVector(start.x, start.y + l);
+//       break;
+//     case 2:
+//       end = createVector(start.x - l, start.y);
+//       break;
+//     case 3:
+//       end = createVector(start.x, start.y - l);
+//       break;
+//   }
+
+//   line(start.x, start.y, end.x, end.y);
+
+//   state = (state + 1) % 4;
+//   draw_line(end, l * shrink, state);
+// }
+
+function mousePressed() {
+  save("frame.webp");
 }
